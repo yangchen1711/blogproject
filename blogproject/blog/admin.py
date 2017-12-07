4a801e8b6cfff53ed0e593e2d66d342d4355e29e
yangchen1711 <yangchen1711@163.com>
2017-12-07 22:19:54 +0800

    增加模板，后台加入文章

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -1,3 +1,12 @@
+# coding:utf-8
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'create_time', 'modified_time', 'category', 'author']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,14 @@
+# -*- coding:utf-8 -*-
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from models import Post
 # Create your views here.
+
+
+def index(request):
+    # return HttpResponse('欢迎访问我的博客首页！')
+    # return render(request, 'blog/index.html', context={'title': '我的博客首页', 'welcome': '欢迎访问我的博客首页！'})
+
+    post_list = Post.objects.all().order_by('create_time')
+    # print post_list
+    return render(request, 'blog/index.html', context={'post_list': post_list})
