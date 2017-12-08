a85e535336d296b46801674f4fac93222b6350d8
yangchen1711 <yangchen1711@163.com>
2017-12-08 20:31:41 +0800

    完成详情页及代码高亮

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from models import Post
+import markdown
 # Create your views here.
 
 
@@ -16,4 +17,10 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
