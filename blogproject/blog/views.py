b059ff61da28754a8418c2413f393c84b30def7d
yangchen1711 <yangchen1711@163.com>
2017-12-08 23:43:07 +0800

    加入评论应用，尚需改进

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from models import Post, Category
 import markdown
+from comments.forms import CommentForm
 # Create your views here.
 
 
@@ -23,7 +24,13 @@ def detail(request, pk):
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {'post': post,
+               'form': form,
+               'comment_list': comment_list
+    }
+    return render(request, 'blog/detail.html', context=context)
 
 
 def archives(request, year, month):
