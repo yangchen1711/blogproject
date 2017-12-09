403b558011d9b42060faddf3cdc8236c3f0c8404
yangchen1711 <yangchen1711@163.com>
2017-12-09 14:17:48 +0800

    完成标签

--- blogproject/blog/templatetags/blog_tags.py
+++ blogproject/blog/templatetags/blog_tags.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 from django import template
+from django.db.models.aggregates import Count
 
 register = template.Library()
 
@@ -19,4 +20,10 @@ def archives():
 
 @register.assignment_tag()
 def get_categories():
-    return Category.objects.all()
+    # return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+
+@register.assignment_tag()
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
